fb4ec385d6b3c59999ba64f7f840e9c61110d631
Ahmed K. A <ahmadkabdullah@pm.me>
2023-06-09 23:42:35 +0300

    Improve styling for system page and inject service data

--- src/app/pages/system/system.component.ts
+++ src/app/pages/system/system.component.ts
@@ -1,7 +1,15 @@
 import { Component } from "@angular/core";
+import { IProduct } from "src/app/models/product.model";
+import { SystemService } from "src/app/services/system.service";
 
 @Component({
 	selector: "app-system",
 	templateUrl: "./system.component.html",
 })
-export class SystemComponent {}
+export class SystemComponent {
+	products: IProduct[];
+
+	constructor(systemService: SystemService) {
+		this.products = systemService.products;
+	}
+}
